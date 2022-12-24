be122f8bde86c03546e867268c3addf388c8f06f
Gaurav Bhadauria <gauravbhadauria@Gauravs-MacBook-Pro.local>
2022-12-24 15:58:12 +0530

    side naviagtion

---
+++ src/common/CommonCard.js
@@ -0,0 +1,67 @@
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const CommonCard = ({isNew, count, newColor, title, icon, bgColor,onClick}) => {
+  return (
+    <View style={{width: '100%', height: 70, justifyContent: 'center'}}>
+      <TouchableOpacity
+        style={{
+          width: '96%',
+          height: 60,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderTopRightRadius: 30,
+          borderBottomRightRadius: 30,
+          backgroundColor: bgColor ? bgColor : '#fff',
+        }} onPress={()=>{
+onClick()
+        }}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image
+            source={icon}
+            style={{
+              width: 24,
+              height: 24,
+              marginLeft: 10,
+              tintColor: bgColor ? 'red' : '#6D6D6D',
+            }}
+          />
+          <Text
+            style={{
+              marginLeft: 20,
+              fontWeight: '600',
+              color: bgColor ? 'red' : '#6D6D6D',
+              fontSize: 16,
+            }}>
+            {title}
+          </Text>
+        </View>
+        {count && (
+          <View
+            style={{
+              backgroundColor: newColor ? newColor : bgColor ? bgColor : '#fff',
+              height: 35,
+              borderRadius: 20,
+              paddingTop: 10,
+              paddingBottom: 10,
+              paddingLeft: 20,
+              paddingRight: 20,
+              marginRight: 10,
+            }}>
+            <Text
+              style={{
+                color: isNew ? '#fff' : bgColor ? 'red' : '#000',
+                fontWeight: '600',
+                fontSize: 16,
+              }}>
+              {count}
+            </Text>
+          </View>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CommonCard;
--- src/drawer/CustomDrawer.js
+++ src/drawer/CustomDrawer.js
@@ -1,11 +1,147 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, FlatList} from 'react-native';
 import React from 'react';
-
-const CustomDrawer = () => {
+import CommonCard from '../common/CommonCard';
+const data = [
+  {
+    title: 'Starred',
+    icon: require('../star.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Snoozed',
+    icon: require('../snooze.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Important',
+    icon: require('../checkbox.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Sent',
+    icon: require('../send.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Scheduled',
+    icon: require('../checkbox.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Outbox',
+    icon: require('../checkbox.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Drafts',
+    icon: require('../checkbox.png'),
+    isNew: false,
+    count: 2,
+  },
+  {
+    title: 'Archieved',
+    icon: require('../checkbox.png'),
+    isNew: false,
+    count: 2,
+  },
+];
+const CustomDrawer = ({navigation}) => {
   return (
-    <View style={{backgroundColor: 'purple', flex: 1}}>
-      <Text>CustomDrawer</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{backgroundColor: 'white', flex: 1}}>
+        <Text
+          style={{
+            color: 'red',
+            fontSize: 27,
+            fontWeight: '700',
+            marginLeft: 20,
+          }}>
+          Gmail
+        </Text>
+        <View
+          style={{
+            width: '100%',
+            marginTop: 20,
+            height: 70,
+            borderTopWidth: 0.2,
+            borderBottomWidth: 0.2,
+            borderBottomColor: '#C7C7C7',
+            borderTopColor: '#C7C7C7',
+          }}>
+          <CommonCard
+            icon={require('../checkbox.png')}
+            count={''}
+            title={'All Inboxes'}
+            onClick={() => {
+              navigation.closeDrawer();
+            }}
+          />
+        </View>
+        <CommonCard
+          icon={require('../checkbox.png')}
+          count={'10+'}
+          bgColor={'#FFE4E4'}
+          title={'Primary'}
+          onClick={() => {
+            navigation.closeDrawer();
+          }}
+        />
+        <CommonCard
+          icon={require('../checkbox.png')}
+          count={'10+'}
+          newColor={'green'}
+          isNew={true}
+          title={'Social'}
+          onClick={() => {
+            navigation.closeDrawer();
+          }}
+        />
+        <CommonCard
+          icon={require('../checkbox.png')}
+          count={'10+'}
+          newColor={'blue'}
+          isNew={true}
+          title={'Promotions'}
+          onClick={() => {
+            navigation.closeDrawer();
+           
+           
+          }}
+        />
+        <Text
+          style={{
+            marginTop: 20,
+            marginLeft: 20,
+            fontSize: 17,
+            fontWeight: '700',
+            color: '#8e8e8e',
+          }}>
+          ALL LABELS
+        </Text>
+        <FlatList
+          data={data}
+          renderItem={({item, index}) => {
+            return (
+              <CommonCard
+                title={item.title}
+                icon={item.icon}
+                count={item.count + '+'}
+                onClick={() => {
+                  navigation.closeDrawer();
+                  alert('title :' + item.title);
+                }}
+              />
+            );
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
